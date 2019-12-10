afb1e4fa5d3a79eb1d270a36bd3c15cfd8d2487e
Student <example@example.com>
2019-12-10 14:53:16 -0800

    refactors code and adds File.open

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,8 +1,17 @@
 #!/usr/bin/env ruby
 
 class Letters
-  def initialize(letter_array)
-    @letter_array = letter_array
+  def initialize(word)
+    @letter_array = word.upcase.split('')
+    @letter_values = {
+      1=>/[AEIOULNRST]/,
+      2=>/[DG]/,
+      3=>/[BCMP]/,
+      4=>/[FHVWY]/,
+      5=>/[K]/,
+      8=>/[JX]/,
+      10=>/[QZ]/
+    }
   end
 
   def letters
@@ -10,24 +19,19 @@ class Letters
   end
 
   def scrabble_score
-    letter_values = {
-      1=>['A','E','I','O','U','L','N','R','S','T'],
-      2=>['D','G'],
-      3=>['B','C','M','P'],
-      4=>['F','H','V','W','Y'],
-      5=>['K'],
-      8=>['J','X'],
-      10=>['Q','Z']
-    }
     score = 0
-    @letter_array.each {|letter|
-      letter_values.each {|key, value|
-        if value.include?(letter)
-          score += key
-        end
+    @letter_array.each { |letter|
+      @letter_values.each { |key, value|
+        if letter =~ value then score += key end
       }
     }
     score
+  end
+
+  def open_and_score(file)
+    file = File.open(file).read.chomp
+    @letter_array = file.upcase.split('').keep_if { |c| c =~ /[A-Z]/ }
+    self.scrabble_score()
   end
 
 end
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -3,18 +3,23 @@ require('scrabble')
 
 describe('#Letters') do
   it('will store array of letters passed to initialize function') do
-    letters = Letters.new(['K'])
+    letters = Letters.new('k')
     expect(letters.letters()).to(eq(['K']))
   end
 
   it 'will return value of five when letters contains only "K"' do
-    letters = Letters.new(['K'])
+    letters = Letters.new('K')
     expect(letters.scrabble_score()).to(eq(5))
   end
 
   it 'will return value of five when letters contains "K","Q","A","F"' do
-    letters = Letters.new(["K","Q","A","F"])
+    letters = Letters.new("KQAF")
     expect(letters.scrabble_score()).to(eq(20))
+  end
+
+  it 'loads and scores Hello There!!!' do
+    letters = Letters.new("KQAF")
+    expect(letters.open_and_score("./tester.txt")).to(eq(16))
   end
 
 end
